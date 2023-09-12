241d114a17e7f4dd2bbf9e96bc54ec8426418aaa
Julia Machina <122980191+machinajg@users.noreply.github.com>
2023-09-12 12:45:04 +0300

    Add files via upload
    SQL Homework 6 is added

---
+++ seminar_6_homework.sql
@@ -0,0 +1,179 @@
+DROP DATABASE IF EXISTS lesson_4;
+CREATE DATABASE lesson_4;
+USE lesson_4;
+
+-- Задача 1. Создайте таблицу users_old, аналогичную таблице users.
+-- Создайте процедуру, с помощью которой можно переместить любого (одного) пользователя из таблицы users в таблицу users_old. 
+-- (использование транзакции с выбором commit или rollback – обязательно).
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY, -- SERIAL = BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
+    firstname VARCHAR(50),
+    lastname VARCHAR(50) COMMENT 'Фамилия',
+    email VARCHAR(120) UNIQUE
+);
+
+INSERT INTO users (id, firstname, lastname, email) VALUES 
+(1, 'Reuben', 'Nienow', 'arlo50@example.org'),
+(2, 'Frederik', 'Upton', 'terrence.cartwright@example.org'),
+(3, 'Unique', 'Windler', 'rupert55@example.org'),
+(4, 'Norene', 'West', 'rebekah29@example.net'),
+(5, 'Frederick', 'Effertz', 'von.bridget@example.net'),
+(6, 'Victoria', 'Medhurst', 'sstehr@example.net'),
+(7, 'Austyn', 'Braun', 'itzel.beahan@example.com'),
+(8, 'Jaida', 'Kilback', 'johnathan.wisozk@example.com'),
+(9, 'Mireya', 'Orn', 'missouri87@example.org'),
+(10, 'Jordyn', 'Jerde', 'edach@example.com');
+
+SELECT * FROM users;
+
+DROP TABLE IF EXISTS users_old;
+CREATE TABLE users_old (
+	id SERIAL PRIMARY KEY, -- SERIAL = BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
+    firstname VARCHAR(50),
+    lastname VARCHAR(50) COMMENT 'Фамилия',
+    email VARCHAR(120) UNIQUE
+);
+
+-- Процедура:
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS move_users;
+CREATE PROCEDURE  move_users (IN user_id INT) 
+	DETERMINISTIC
+BEGIN
+INSERT INTO users_old (firstname,lastname,email) 
+SELECT firstname, lastname, email 
+	FROM users 
+	WHERE users.id = user_id;
+DELETE FROM users 
+	WHERE id = user_id;
+COMMIT;
+END//
+DELIMITER ;
+
+CALL move(5);
+
+SELECT * FROM users_old;
+SELECT * FROM users;
+
+
+-- Задача 2.  Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток.
+-- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро",
+-- с 12:00 до 18:00 функция должна возвращать фразу "Добрый день",
+-- с 18:00 до 00:00 — "Добрый вечер",
+-- с 00:00 до 6:00 — "Доброй ночи".
+
+DELIMITER //
+CREATE FUNCTION hello() 
+	RETURNS VARCHAR(20)
+	DETERMINISTIC
+BEGIN
+DECLARE greetings VARCHAR(20);
+SELECT CASE 
+	WHEN CURRENT_TIME >= '12:00:00' AND  CURRENT_TIME < '18:00:00' THEN 'Добрый день'
+	WHEN CURRENT_TIME >= '06:00:00' AND  CURRENT_TIME < '12:00:00' THEN 'Доброе утро'
+	WHEN CURRENT_TIME >= '00:00:00' AND  CURRENT_TIME < '06:00:00' THEN 'Доброй ночи'
+	ELSE 'Добрый вечер'
+END INTO greetings;
+RETURN greetings;
+END//
+DELIMITER ;
+
+SELECT hello();
+
+
+-- Задание 3. Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, communities и messages
+--  в таблицу logs помещается время и дата создания записи, название таблицы, идентификатор первичного ключа.
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+	id SERIAL PRIMARY KEY,
+	from_user_id BIGINT UNSIGNED NOT NULL,
+    to_user_id BIGINT UNSIGNED NOT NULL,
+    body TEXT,
+    created_at DATETIME DEFAULT NOW(),
+    FOREIGN KEY (from_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (to_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+INSERT INTO messages  (from_user_id, to_user_id, body, created_at) VALUES
+(1, 2, 'Voluptatem ut quaerat quia. Pariatur esse amet ratione qui quia. In necessitatibus reprehenderit et. Nam accusantium aut qui quae nesciunt non.',  DATE_ADD(NOW(), INTERVAL 1 MINUTE)),
+(2, 1, 'Sint dolores et debitis est ducimus. Aut et quia beatae minus. Ipsa rerum totam modi sunt sed. Voluptas atque eum et odio ea molestias ipsam architecto.',  DATE_ADD(NOW(), INTERVAL 3 MINUTE)),
+(3, 1, 'Sed mollitia quo sequi nisi est tenetur at rerum. Sed quibusdam illo ea facilis nemo sequi. Et tempora repudiandae saepe quo.',  DATE_ADD(NOW(), INTERVAL 5 MINUTE)),
+(4, 1, 'Quod dicta omnis placeat id et officiis et. Beatae enim aut aliquid neque occaecati odit. Facere eum distinctio assumenda omnis est delectus magnam.',  DATE_ADD(NOW(), INTERVAL 11 MINUTE)),
+(1, 5, 'Voluptas omnis enim quia porro debitis facilis eaque ut. Id inventore non corrupti doloremque consequuntur. Molestiae molestiae deleniti exercitationem sunt qui ea accusamus deserunt.',  DATE_ADD(NOW(), INTERVAL 12 MINUTE)),
+(1, 6, 'Rerum labore culpa et laboriosam eum totam. Quidem pariatur sit alias. Atque doloribus ratione eum rem dolor vitae saepe.',  DATE_ADD(NOW(), INTERVAL 14 MINUTE)),
+(1, 7, 'Perspiciatis temporibus doloribus debitis. Et inventore labore eos modi. Quo temporibus corporis minus. Accusamus aspernatur nihil nobis placeat molestiae et commodi eaque.',  DATE_ADD(NOW(), INTERVAL 15 MINUTE)),
+(8, 1, 'Suscipit dolore voluptas et sit vero et sint. Rem ut ratione voluptatum assumenda nesciunt ea. Quas qui qui atque ut. Similique et praesentium non voluptate iure. Eum aperiam officia quia dolorem.',  DATE_ADD(NOW(), INTERVAL 21 MINUTE)),
+(9, 3, 'Et quia libero aut vitae minus. Rerum a blanditiis debitis sit nam. Veniam quasi aut autem ratione dolorem. Sunt quo similique dolorem odit totam sint sed.',  DATE_ADD(NOW(), INTERVAL 22 MINUTE)),
+(10, 2, 'Praesentium molestias quia aut odio. Est quis eius ut animi optio molestiae. Amet tempore sequi blanditiis in est.',  DATE_ADD(NOW(), INTERVAL 25 MINUTE)),
+(8, 3, 'Molestiae laudantium quibusdam porro est alias placeat assumenda. Ut consequatur rerum officiis exercitationem eveniet. Qui eum maxime sed in.',  DATE_ADD(NOW(), INTERVAL 27 MINUTE)),
+(8, 1, 'Quo asperiores et id veritatis placeat. Aperiam ut sit exercitationem iste vel nisi fugit quia. Suscipit labore error ducimus quaerat distinctio quae quasi.',  DATE_ADD(NOW(), INTERVAL 28 MINUTE)),
+(8, 1, 'Earum sunt quia sed harum modi accusamus. Quia dolor laboriosam asperiores aliquam quia. Sint id quasi et cumque qui minima ut quo. Autem sed laudantium officiis sit sit.',  DATE_ADD(NOW(), INTERVAL 33 MINUTE)),
+(4, 1, 'Aut enim sint voluptas saepe. Ut tenetur quos rem earum sint inventore fugiat. Eaque recusandae similique earum laborum.',  DATE_ADD(NOW(), INTERVAL 35 MINUTE)),
+(4, 1, 'Nisi rerum officiis officiis aut ad voluptates autem. Dolor nesciunt eum qui eos dignissimos culpa iste. Atque qui vitae quos odit inventore eum. Quam et voluptas quia amet.',  DATE_ADD(NOW(), INTERVAL 35 MINUTE)),
+(4, 1, 'Consequatur ut et repellat non voluptatem nihil veritatis. Vel deleniti omnis et consequuntur. Et doloribus reprehenderit sed earum quas velit labore.',  DATE_ADD(NOW(), INTERVAL 37 MINUTE)),
+(2, 1, 'Iste deserunt in et et. Corrupti rerum a veritatis harum. Ratione consequatur est ut deserunt dolores.',  DATE_ADD(NOW(), INTERVAL 37 MINUTE)),
+(8, 1, 'Dicta non inventore autem incidunt accusamus amet distinctio. Aut laborum nam ab maxime. Maxime minima blanditiis et neque. Et laboriosam qui at deserunt magnam.',  DATE_ADD(NOW(), INTERVAL 41 MINUTE)),
+(8, 1, 'Amet ad dolorum distinctio excepturi possimus quia. Adipisci veniam porro ipsum ipsum tempora est blanditiis. Magni ut quia eius qui.',  DATE_ADD(NOW(), INTERVAL 42 MINUTE)),
+(8, 1, 'Porro aperiam voluptate quo eos nobis. Qui blanditiis cum id eos. Est sit reprehenderit consequatur eum corporis. Molestias quia quo sit architecto aut.',  DATE_ADD(NOW(), INTERVAL 50 MINUTE));
+
+DROP TABLE IF EXISTS communities;
+CREATE TABLE communities(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(150),
+    INDEX communities_name_idx(name)
+);
+
+INSERT INTO `communities` (name) 
+VALUES ('atque'), ('beatae'), ('est'), ('eum'), ('hic'), ('nemo'), ('quis'), ('rerum'), ('tempora'), ('voluptas');
+
+-- пользователи сообщества
+DROP TABLE IF EXISTS users_communities;
+CREATE TABLE users_communities(
+	user_id BIGINT UNSIGNED NOT NULL,
+	community_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (user_id, community_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (community_id) REFERENCES communities(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+INSERT INTO users_communities (user_id, community_id) 
+VALUES 
+(1, 1), (1, 2), (1, 3),
+(2, 1), (2, 2), 
+(3, 1), (3, 2), (3, 10), (3, 5), (3, 8),
+(4, 1), (4, 2), (4, 3), (4, 8),
+(5, 1), (5, 2), (5, 3), (5, 6), (5, 8), (5, 10),
+(6, 1), (6, 2), (6, 3), (6, 6),
+(7, 1), (7, 2), (7, 3), (7, 8), (7, 7), (7, 6),
+(8, 1), (8, 2), (8, 3), (8, 5), (8, 7), (8, 9),
+(9, 1), (9, 2),  
+(10, 1), (10, 10);
+
+
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (
+    created_at DATETIME DEFAULT now(),
+    table_name VARCHAR(20) NOT NULL,
+    primary_key_id INT UNSIGNED NOT NULL
+)  ENGINE=ARCHIVE;
+
+CREATE 
+    TRIGGER  users_log
+ AFTER INSERT ON users FOR EACH ROW 
+    INSERT INTO logs SET table_name = 'users' , primary_key_id = NEW.id;
+CREATE 
+    TRIGGER  communities_log
+ AFTER INSERT ON communities FOR EACH ROW 
+    INSERT INTO logs SET table_name = 'communities' , primary_key_id = NEW.id;
+CREATE 
+    TRIGGER  messages_log
+AFTER INSERT ON messages FOR EACH ROW 
+    INSERT INTO logs SET table_name = 'messages' , primary_key_id = NEW.id;
+
+SELECT * FROM logs;
+
